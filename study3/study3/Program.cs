628393fd47182e1724a79b54e99966b7cda5a79b
Accdo <khyeong1002@gmail.com>
2025-02-21 10:41:32 +0900

    study3

---
+++ study3/study3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace study3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //리터럴 : 코드에서 고정된 값을 의미한다.
+            int hp = 100;    
+            double att = 56.7;   
+            string name = "홍길동";     
+            char level = 'S';
+
+            Console.WriteLine(hp); 
+            Console.WriteLine(att); 
+            Console.WriteLine(name);
+            Console.WriteLine(level); 
+        }
+    }
+}

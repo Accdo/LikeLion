00f784228fd6033992376285c18f336633b8df3d
Accdo <khyeong1002@gmail.com>
2025-02-28 17:50:51 +0900

    Shooting_Console

---
+++ Study17/Study17/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Study17
+{
+    class Marin
+    {
+        public string name;
+        public int mineral;
+
+        public Marin()
+        {
+            this.name = "마린";
+            this.mineral = 50;
+        }
+
+        public Marin(string name, int mineral)
+        {
+            this.name = name;
+            this.mineral = mineral;
+        }
+        public void ShowInfo()
+        {
+            Console.WriteLine($"이름 : {name} , 미네랄 : {mineral}");
+        }
+    }
+
+    class Barracks
+    {
+        public string name;
+        public int mineral;
+
+        public Barracks()
+        {
+            this.name = "배럭";
+            this.mineral = 150;
+        }
+        public Barracks(string name, int mineral)
+        {
+            this.name = name;
+            this.mineral = mineral;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"이름 : {name} , 미네랄 : {mineral}");
+        }
+    }
+
+    class Mineral
+    {
+        public int MineralCount;
+        public int num;
+        
+        public Mineral()
+        {
+            MineralCount = 1500;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"미네랄 개수 : {MineralCount}");
+        }
+    }
+
+    class Game
+    {
+        public static int mineral;
+        public static int gas;
+        public static int charCount;
+
+        public static void ShowInfo()
+        {
+            Console.WriteLine($"미네랄 {mineral} 가스 {gas} 인구 수 {charCount}");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game.mineral = 50;
+            Game.gas = 0;
+            Game.charCount = 10;
+            Game.ShowInfo();
+
+            Marin m1 = new Marin();
+            Marin m2 = new Marin("SCV", 50);
+            Barracks barracks = new Barracks();
+
+            Mineral[] mineral = new Mineral[7];
+
+            for(int i = 0; i < mineral.Length; i++)
+            {
+                mineral[i] = new Mineral();
+                mineral[i].ShowInfo();
+            }
+
+
+            m1.ShowInfo();
+            m2.ShowInfo();
+            barracks.ShowInfo();
+
+        }
+    }
+}
---
+++ Study18/Study18/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Study18
+{
+    //class Person
+    //{
+    //    private string name;
+
+
+    //    public void SetName(string newName)
+    //    {
+    //        name = newName;
+    //    }
+    //    public string GetName()
+    //    {
+    //        return name;
+    //    }
+    //}
+
+    //class Person
+    //{
+    //    private string name; //내부변수
+
+    //    public string Name //프로퍼티
+    //    {
+    //        get { return name; } //Getter
+    //        set { name = value; } //Setter
+    //    }
+    //}
+
+    //class Person
+    //{
+    //    private int count = 100;
+
+    //    public string Name { get; set; }
+
+    //    public int Count
+    //    {
+    //        get { return count; }
+    //    }
+    //    public float Balance { get; private set; }
+    //    public void AddBal()
+    //    {
+    //        Balance += 100;
+    //    }
+
+    //}   
+
+    class Marin
+    {
+        public string Name { get; private set; } = "마린";
+        public int Mineral { get; set; } = 50;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Person p = new Person();
+            //p.SetName("홍길동");
+            //Console.WriteLine(p.GetName());
+
+            //Person p = new Person();
+            //p.Name = "홍길동";
+            //p.AddBal();
+            //Console.WriteLine("이름 : " + p.Name + " Count : " + p.Count + " Balance : " + p.Balance);
+
+            Marin m = new Marin();
+
+            Console.WriteLine("이름 : " + m.Name + ", 미네랄 : " + m.Mineral);
+        }
+    }
+}
